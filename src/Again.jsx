56db5c11cc416dbe01a5f8f8fc33111c65269dc0
vikash <vikash.tomar.india@gmail.com>
2023-08-22 23:43:47 +0530

    editing ...

--- src/Again.jsx
+++ src/Again.jsx
@@ -4,13 +4,27 @@
  function Again() {
   const[task,setTask] = useState([])
   const[inptval,setInputVal] = useState('')
+  const[editing,setEditIndex] = useState(-1)
 
   function handleadd()
   {
     setTask([...task,inptval])
     setInputVal("")
   }
+  function handledlt(index)
+  {
+    const updatedval = [...task]
+    updatedval.splice(index,1)
+    setTask(updatedval)
+    console.log("dlt")
+  }
 
+  function handleEditSave(index, newValue) {
+    const editedVal = [...task];
+    editedVal[index] = newValue;
+    setTask(editedVal);
+    setEditIndex(-1); // Stop editing mode
+  }
    return (
      <div>
 
@@ -18,12 +32,14 @@
       onChange={e=> setInputVal(e.target.value) } />
 
       <button onClick={handleadd}>Add</button>
+
  
         {task.map((val)=>{
           return(
             <ul>
               <li>
-                {val}
+                {val}  <button onClick={handledlt}> Remove item</button> 
+                <button onClick={handleEditSave}> Edit</button>
               </li>
             </ul>
           )
