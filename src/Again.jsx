f5a7192d573cea36c1f592219053ff02a5426afc
vikash <vikash.tomar.india@gmail.com>
2023-08-23 16:40:41 +0530

    update edit function

--- src/Again.jsx
+++ src/Again.jsx
@@ -1,54 +1,66 @@
- import React, { useState } from 'react'
+/** @format */
 
- 
- function Again() {
-  const[task,setTask] = useState([])
-  const[inptval,setInputVal] = useState('')
-  const[editing,setEditIndex] = useState(-1)
+import React, { useState } from "react";
 
-  function handleadd()
-  {
-    setTask([...task,inptval])
-    setInputVal("")
+function Again() {
+  const [task, setTask] = useState([]);
+  const [inptval, setInputVal] = useState("");
+  const [editIndex, setEditIndex] = useState(0);
+  const [isEdit, setIsEdit] = useState(false);
+
+
+//----------------------ADD FUNCTION--------------------------//
+  function handleadd() {
+    setTask([...task, inptval]);
+    setInputVal("");
+
+    if (isEdit) {
+      const updatedval = [...task];
+      updatedval.splice(editIndex, 1, inptval);
+      setTask(updatedval);
+      setIsEdit(false);
+    }
   }
-  function handledlt(index)
-  {
-    const updatedval = [...task]
-    updatedval.splice(index,1)
-    setTask(updatedval)
-    console.log("dlt")
+//----------------------Delete Function-------------------------//
+  function handledlt(index) {
+    const updatedval = [...task];
+    updatedval.splice(index, 1);
+    setTask(updatedval);
+    console.log("dlt");
   }
 
-  function handleEditSave(index, newValue) {
-    const editedVal = [...task];
-    editedVal[index] = newValue;
-    setTask(editedVal);
-    setEditIndex(-1); // Stop editing mode
+  //--------------------EDIT FUNCTION-----------------------//
+  function handleEdit(e, val, index) {
+    e.preventDefault();
+    setInputVal(val);
+    setEditIndex(index);
+    setIsEdit(true);
   }
-   return (
-     <div>
 
-      <input type="text" placeholder='Enter value' value={inptval} 
-      onChange={e=> setInputVal(e.target.value) } />
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Enter value"
+        value={inptval}
+        onChange={(e) => setInputVal(e.target.value)}
+      />
 
       <button onClick={handleadd}>Add</button>
 
- 
-        {task.map((val)=>{
-          return(
-            <ul>
-              <li>
-                {val}  <button onClick={handledlt}> Remove item</button> 
-                <button onClick={handleEditSave}> Edit</button>
-              </li>
-            </ul>
-          )
+      <ul>
+        {task.map((val, index) => {
+          return (
+            <li>
+              {" "}
+              {val} <button onClick={handledlt}> Remove item</button>
+              <button onClick={(e) => handleEdit(e, val, index)}> Edit</button>
+            </li>
+          );
         })}
-    
+      </ul>
+    </div>
+  );
+}
 
-     </div>
-
-   )
- }
- 
- export default Again
+export default Again;
